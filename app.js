970f43f4678dce321c3b8620a3c579ddb4ca033b
ElmoElmaz <elmazelmaz79@gmail.com>
2024-04-03 09:55:25 +0200

    New Port 6000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const app = express(); // za lokalna upotreba 
-const PORT = 5000;
+const PORT = 6000;
 
 
 //Midleware
